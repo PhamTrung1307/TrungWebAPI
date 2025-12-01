95ec743ec5adb0ded5d30e1ee66e7edd83218e4d
PhamTrung1307 <phamnguyenanhtrung2003@gmail.com>
2025-12-01 11:02:13 +0700

    push

---
+++ src/ClassLibrary1/Domain/Content/Post.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Domain.Content
+{   
+    [Table("Posts")]
+    [Index(nameof(Slug), IsUnique = true)]
+    public class Post
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public required string Name { get; set; }
+        [Required]
+        [MaxLength(250)]
+        [Column(TypeName = "varchar(250)")]
+        public required string Slug { get; set; }
+        [MaxLength(500)]
+        public string? Description { get; set; } 
+        [Required]
+        public Guid CategoryID { get; set; }
+        [MaxLength(500)]
+        public string? Thumbnail { get; set; }
+        public string? Content { get; set; }
+        [MaxLength(250)]
+        public Guid AuthorUserID { get; set; }
+        public string? Source { get; set; }
+        public string? Tags { get; set; }
+        [MaxLength(160)]
+        public string? SeoDescription { get; set; }
+        public int ViewCount { get; set; } 
+        public bool IsPaid{ get; set; }
+        public double RoyaltyAmount { get; set; }
+        public PostStatus Status { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+    public enum PostStatus
+    {
+        Draft =1,
+        cancelled=2,
+        waitingForApproval=3,
+        rejected=4,
+        waitingForPublish=5,
+        published = 6
+    }
+}
--- src/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ src/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c68ca4ec6eb4674063fe0f66f7ea0610c7b397")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
