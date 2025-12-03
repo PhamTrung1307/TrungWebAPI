701a844b041bb4f1e885f9e115671ea3241dab57
PhamTrung1307 <phamnguyenanhtrung2003@gmail.com>
2025-12-03 16:07:42 +0700

    push

--- src/ClassLibrary1/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ src/ClassLibrary1/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0663880ffa31df9c1c86f7c7201adaf7d843da00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933f8911727985baf9779dbdd2e1f7dfff028816")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
